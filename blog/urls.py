70d7a18000a6c6771c4981fc0bd3da72da0ec7fd
victorma123 <victorzambranosanchez@gmail.com>
2025-03-18 20:23:57 -0400

    publication detail done

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PublicationListView
+from .views import PublicationListView, PublicationDetailView
 
 urlpatterns = [
     path('', PublicationListView.as_view(), name='publication-list'),
+    path( 'publication/<int:pk>/', PublicationDetailView.as_view(), name='publication-detail'), #(int:pk) es un parametro que se pasa a la vista y representa el numero de la llave primaria del objeto que se quiere ver   
+    
 ]
