bd4ae9bdd1bb07ecea4c0086f544289d965fb9f3
victorma123 <victorzambranosanchez@gmail.com>
2025-03-18 20:44:57 -0400

    publication create v1 without link

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import PublicationListView, PublicationDetailView
+from .views import (
+                    PublicationListView, 
+                    PublicationDetailView, 
+                    PublicationCreateView,
+                    )
 
 urlpatterns = [
     path('', PublicationListView.as_view(), name='publication-list'),
     path( 'publication/<int:pk>/', PublicationDetailView.as_view(), name='publication-detail'), #(int:pk) es un parametro que se pasa a la vista y representa el numero de la llave primaria del objeto que se quiere ver   
-    
+    path('publication/new/', PublicationCreateView.as_view(), name='publication-create')
 ]
