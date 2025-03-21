288b735ce590fa17c2f4c9cc628b1c52b303f407
victorma123 <victorzambranosanchez@gmail.com>
2025-03-21 11:42:47 -0400

    publication update done

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,12 @@ from .views import (
                     PublicationListView, 
                     PublicationDetailView, 
                     PublicationCreateView,
+                    PublicationUpdateView
                     )
 
 urlpatterns = [
     path('', PublicationListView.as_view(), name='publication-list'),
     path( 'publication/<int:pk>/', PublicationDetailView.as_view(), name='publication-detail'), #(int:pk) es un parametro que se pasa a la vista y representa el numero de la llave primaria del objeto que se quiere ver   
-    path('publication/new/', PublicationCreateView.as_view(), name='publication-create')
+    path('publication/new/', PublicationCreateView.as_view(), name='publication-create'),
+    path('publication/<int:pk>/update/', PublicationUpdateView.as_view(), name='publication-update'),
 ]
